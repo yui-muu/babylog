8259b7015cb30c9cafdd51f8037dca8f01fb9de3
yui.murayama <ravu34zu@yahoo.co.jp>
2020-07-20 12:04:42 +0000

    Seeder

--- database/seeds/AveragesTableSeeder.php
+++ database/seeds/AveragesTableSeeder.php
@@ -11,79 +11,200 @@ class AveragesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('averages')->where('id', 1)
-        ->update(['perday' => '25',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '0',
+            'age_to' => '30',
+            'weight' => '3.0',
+            'height' => '48.7',
+            'perday' => '25',
         ]);
-        DB::table('averages')->where('id', 2)
-        ->update(['perday' => '25',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '31',
+            'age_to' => '60',
+            'weight' => '4.8',
+            'height' => '55.5',
+            'perday' => '25',
         ]);
-        DB::table('averages')->where('id', 3)
-        ->update(['perday' => '25',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '61',
+            'age_to' => '90',
+            'weight' => '5.8',
+            'height' => '59.0',
+            'perday' => '25',
         ]);
-        DB::table('averages')->where('id', 4)
-        ->update(['perday' => '15',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '91',
+            'age_to' => '120',
+            'weight' => '6.6',
+            'height' => '61.9',
+            'perday' => '15',
         ]);
-        DB::table('averages')->where('id', 5)
-        ->update(['perday' => '15',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '121',
+            'age_to' => '150',
+            'weight' => '7.2',
+            'height' => '64.3',
+            'perday' => '15',
         ]);
-        DB::table('averages')->where('id', 6)
-        ->update(['perday' => '15',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '151',
+            'age_to' => '180',
+            'weight' => '7.7',
+            'height' => '66.2',
+            'perday' => '15',
         ]);
-        DB::table('averages')->where('id', 7)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '181',
+            'age_to' => '210',
+            'weight' => '8.0',
+            'height' => '67.9',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 8)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '211',
+            'age_to' => '240',
+            'weight' => '8.3',
+            'height' => '69.3',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 9)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '241',
+            'age_to' => '270',
+            'weight' => '8.5',
+            'height' => '70.6',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 10)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '271',
+            'age_to' => '300',
+            'weight' => '8.7',
+            'height' => '71.8',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 11)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '301',
+            'age_to' => '330',
+            'weight' => '8.9',
+            'height' => '72.9',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 12)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Boy',
+            'age_from' => '331',
+            'age_to' => '365',
+            'weight' => '9.1',
+            'height' => '73.9',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 13)
-        ->update(['perday' => '25',
+        
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '0',
+            'age_to' => '30',
+            'weight' => '2.9',
+            'height' => '48.3',
+            'perday' => '25',
         ]);
-        DB::table('averages')->where('id', 14)
-        ->update(['perday' => '25',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '31',
+            'age_to' => '60',
+            'weight' => '4.5',
+            'height' => '54.5',
+            'perday' => '25',
         ]);
-        DB::table('averages')->where('id', 15)
-        ->update(['perday' => '25',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '61',
+            'age_to' => '90',
+            'weight' => '5.4',
+            'height' => '57.8',
+            'perday' => '25',
         ]);
-        DB::table('averages')->where('id', 16)
-        ->update(['perday' => '15',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '91',
+            'age_to' => '120',
+            'weight' => '6.2',
+            'height' => '60.6',
+            'perday' => '15',
         ]);
-        DB::table('averages')->where('id', 17)
-        ->update(['perday' => '15',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '121',
+            'age_to' => '150',
+            'weight' => '6.7',
+            'height' => '62.9',
+            'perday' => '15',
         ]);
-        DB::table('averages')->where('id', 18)
-        ->update(['perday' => '15',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '151',
+            'age_to' => '180',
+            'weight' => '7.2',
+            'height' => '64.8',
+            'perday' => '15',
         ]);
-        DB::table('averages')->where('id', 19)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '181',
+            'age_to' => '210',
+            'weight' => '7.5',
+            'height' => '66.4',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 20)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '211',
+            'age_to' => '240',
+            'weight' => '7.8',
+            'height' => '67.9',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 21)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '241',
+            'age_to' => '270',
+            'weight' => '8.0',
+            'height' => '69.1',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 22)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '271',
+            'age_to' => '300',
+            'weight' => '8.2',
+            'height' => '70.3',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 23)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '301',
+            'age_to' => '330',
+            'weight' => '8.4',
+            'height' => '71.3',
+            'perday' => '10',
         ]);
-        DB::table('averages')->where('id', 24)
-        ->update(['perday' => '10',
+        DB::table('averages')->insert([
+            'gender' => 'Girl',
+            'age_from' => '331',
+            'age_to' => '365',
+            'weight' => '8.5',
+            'height' => '72.3',
+            'perday' => '10',
         ]);
         
         
-    }
+    } 
 }
